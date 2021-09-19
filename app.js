f4c765809a74c0cbf586d6fa3e3dc42a004d6558
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-09-19 10:30:35 -0600

    added inquirer

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
+const inquirer = require('inquirer')
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
+// const profileDataArgs = process.argv.slice(2);
 
+// const [name, github] = profileDataArgs;
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
+// fs.writeFile('./index.html', generatePage(names, github), err => {
+//   if (err) throw new Error(err);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// notice the lack of partenthese around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-// this..
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-        }
-   
-        console.log('================');
-
-        // is the same as this ..
-        profileDataArr.forEach(profileItem => console.log(profileItem)); 
-};
-
-printProfileData(profileDataArgs);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });

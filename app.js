d5cf5cc7ad27cd1e06f49b3183c4740fecad3d6c
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-10-05 12:08:48 -0600

    added html

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer')
 
 const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template');
 
 // const profileDataArgs = process.argv.slice(2);
 
@@ -151,13 +151,13 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
+   const pageHTML = generatePage(portfolioData);
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+}); 
 });
 
   
 
-// fs.writeFile('./index.html', generatePage(names, github), err => {
-//   if (err) throw new Error(err);
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
